15481c173735000be55d1cf5f1189ab45b66ad85
ernanvr <ernanvr@gmail.com>
2020-09-11 00:47:22 -0600

    Version 0.1

--- registers.c
+++ registers.c
@@ -120,7 +120,7 @@ void exportdata(){
 }
 
 void seekRegister(){
-	CONTACT *current = NULL;
+	CONTACT *current = NULL; 
 	BUFFER buffer;
 	int number;
 	current = global.headNodes;	
@@ -162,9 +162,8 @@ void seekRegister(){
 			while (current)
 			{
 				if(strstr(current->lastName,buffer)){
-					printf("[Id: %d] %s %s | Qty of phone numbers: %d\n",\
-							current->id, current->firstName,current->lastName,\
-							current->phoneQty);
+					printf("[Id: %d] %s %s \n",\
+							current->id, current->firstName,current->lastName);
 					current = current->next;
 				} else {
 					current = current->next;
@@ -184,31 +183,154 @@ void seekRegister(){
 }
 
 void modifyRegister(){
-	CONTACT *previous=NULL, *current=NULL;
+	CONTACT *current=NULL;
+	PHONE phones[10]={0};
 	BUFFER buffer;
-	int id, option;
-	FIRSTNAME tempname;
-	LASTNAME templastName;
-	PHONE phoneNumber[10];
-
-
+	int id=0, option=0;
+	
 	if(global.qtyContacts){
-		previous = global.headNodes;
 		current= global.headNodes;
 
 		setColorText("Bold Red");
+		printf("Bienvenido al submenú de modificación de registros\n");
+		setColorText("Reset");
+		
+		setColorText("Cyan");
 		printf("Introduzca ID del contacto que desea modificar\n");
-		setColorText("Red");
 		captureData(sizeof(NUMBER), &buffer);
 		id = getNumber(buffer);
 		setColorText("Reset");
 
 		while (current) {
 			if(current->id == id){
-				
-			}	
+				break;	
+			} else {
+				current = current->next;
+			}
 		}
 		
+		if (!current) {
+			printf("Register not found\n");
+		} else {
+			printf("El Id pertenece al contacto %s %s\n", current->firstName, current->lastName);
+			setColorText("Cyan");
+			printf("1. Cambiar nombre, 2. Cambiar Apellido, 3. Agregar Número, 4. Eliminar número, 5. Modificar número\
+					, 6. Cancelar\n");
+			captureData(sizeof(NUMBER), &buffer);
+			option = getNumber(buffer);
+			setColorText("Reset");
+
+			switch (option) {
+				case 1:
+					captureData(sizeof(FIRSTNAME), &buffer);
+					strcpy(current->firstName,buffer);
+					break;
+				case 2:
+					captureData(sizeof(LASTNAME), &buffer);
+					strcpy(current->lastName,buffer);
+					break;
+				case 3:
+					switch (current->phoneQty) {
+						case 10:
+							printf("Max qty phones reached\n");
+							break;
+						case 9:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_10, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_10);
+							break;
+						case 8:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_9, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_9);
+							break;
+						case 7:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_8, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_8);
+							break;
+						case 6:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_7, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_7);
+							break;
+						case 5:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_6, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_6);
+							break;
+						case 4:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_5, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_5);
+							break;
+						case 3:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_4, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_4);
+							break;
+						case 2:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_3, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_3);
+							break;
+						case 1:
+							captureData(sizeof(PHONE), &buffer);
+							strcpy(current->phone_2, buffer);
+							current->phoneQty++;
+							printf("Nuevo número de teléfono guardado: %s\n", current->phone_2);
+							break;
+					}
+					break;
+				case 4:
+					current->phoneQty--;
+					break;
+				case 5:
+					printf("Cuál teléfono desea cambiar\n");
+					switch (current->phoneQty) {
+						case 10:
+							strcpy(phones[9],current->phone_10);
+						case 9:
+							strcpy(phones[8],current->phone_9);
+						case 8:
+							strcpy(phones[7],current->phone_8);
+						case 7:
+							strcpy(phones[6],current->phone_7);
+						case 6:
+							strcpy(phones[5],current->phone_6);
+						case 5:
+							strcpy(phones[4],current->phone_5);
+						case 4:
+							strcpy(phones[3],current->phone_4);
+						case 3:
+							strcpy(phones[2],current->phone_3);
+						case 2:
+							strcpy(phones[1],current->phone_2);
+						case 1:
+							strcpy(phones[0],current->phone_1);
+							break;
+
+					for (int i; i<current->phoneQty; i++) {
+						printf("Phone [%d]: %s\n",i+1,phones[i]);
+					}	
+					
+					captureData(sizeof(NUMBER), &buffer);
+					option = getNumber(buffer);
+
+							
+					}
+					break;
+
+			}
+		}
 	} else {
 		printf("Theren't contacts to modify\n");
 	}
@@ -275,4 +397,5 @@ void deleteRegister(){
 	} else {
 		printf("Theren't contacts to modify\n");
 	}
+
 }
